293a446a5313807bc2683659c73383ea6f553490
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-02-05 00:47:29 -0500

    aider: Modify the test to use the number of authors and reviewers specified in `inputs.txt`.
    # Aider chat conversation:
    
    USER: now change the test to use the number of authors and reviewers specified in `inputs.txt`
    
    ASSISTANT: To modify the test in `peerReviewTest.js` to use the number of authors and reviewers specified in `inputs.txt`, we need to adjust the test setup according to the provided inputs. According to `inputs.txt`, there is 1 author and 4 reviewers (though there's a duplicate address for one reviewer, which we'll assume is an error and count as 3 unique reviewers).
    
    Here's how we'll change the test:
    
    peerReviewTest.js
    ```javascript
    <<<<<<< SEARCH
    const [owner, addr1, addr2, addr3] = await ethers.getSigners();
    const PeerReview = await ethers.getContractFactory("PeerReview");
    const peerReview = await PeerReview.deploy([addr1.address, addr2.address, addr3.address], []);
    
    await peerReview.deployed();
    
    const authors = await peerReview.authors();
    expect(authors.length).to.equal(3);
    =======
    const [owner, author, reviewer1, reviewer2, reviewer3] = await ethers.getSigners();
    const PeerReview = await ethers.getContractFactory("PeerReview");
    const peerReview = await PeerReview.deploy([author.address], [reviewer1.address, reviewer2.address, reviewer3.address]);
    
    await peerReview.deployed();
    
    const authors = await peerReview.authors();
    const reviewers = await peerReview.reviewers();
    expect(authors.length).to.equal(1);
    expect(reviewers.length).to.equal(3);
    >>>>>>> REPLACE
    ```
    
    This modification does the following:
    1. Adjusts the signers to reflect 1 author and 3 unique reviewers.
    2. Changes the deployment of the `PeerReview` contract to include 1 author and 3 reviewers, as specified.
    3. Adds an assertion to check the number of reviewers in addition to the number of authors, ensuring both match the specifications in `inputs.txt`.

--- peerReviewTest.js
+++ peerReviewTest.js
@@ -3,13 +3,15 @@ const { ethers } = require("hardhat");
 
 describe("PeerReview Contract", function () {
   it("Should return the correct number of authors after construction", async function () {
-    const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+    const [owner, author, reviewer1, reviewer2, reviewer3] = await ethers.getSigners();
     const PeerReview = await ethers.getContractFactory("PeerReview");
-    const peerReview = await PeerReview.deploy([addr1.address, addr2.address, addr3.address], []);
+    const peerReview = await PeerReview.deploy([author.address], [reviewer1.address, reviewer2.address, reviewer3.address]);
 
     await peerReview.deployed();
 
     const authors = await peerReview.authors();
-    expect(authors.length).to.equal(3);
+    const reviewers = await peerReview.reviewers();
+    expect(authors.length).to.equal(1);
+    expect(reviewers.length).to.equal(3);
   });
 });
