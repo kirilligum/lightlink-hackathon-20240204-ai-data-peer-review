460763a221a8421dfaca5ba47f08a0cb82e500be
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-02-04 22:22:52 -0500

    Update Solidity pragma to version 0.8.9 and add console.log for debugging.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,10 @@
 
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
+
+// We import this library to be able to use console.log
+import "hardhat/console.sol";
 
 
 // This is the main building block for smart contracts.
@@ -11,16 +14,16 @@ contract Token {
     string public name = "My Hardhat Token";
     string public symbol = "MHT";
 
-    // The fixed amount of tokens, stored in an unsigned integer type variable.
+    // The fixed amount of tokens stored in an unsigned integer type variable.
     uint256 public totalSupply = 1000000;
 
     // An address type variable is used to store ethereum accounts.
     address public owner;
 
-    // A mapping is a key/value map. Here we store each account's balance.
+    // A mapping is a key/value map. Here we store each account balance.
     mapping(address => uint256) balances;
 
-    // The Transfer event helps off-chain applications understand
+    // The Transfer event helps off-chain aplications understand
     // what happens within your contract.
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
@@ -37,7 +40,7 @@ contract Token {
     /**
      * A function to transfer tokens.
      *
-     * The `external` modifier makes a function *only* callable from *outside*
+     * The `external` modifier makes a function *only* callable from outside
      * the contract.
      */
     function transfer(address to, uint256 amount) external {
@@ -45,6 +48,15 @@ contract Token {
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        // We can print messages and values using console.log, a feature of
+        // Hardhat Network:
+        console.log(
+            "Transferring from %s to %s %s tokens",
+            msg.sender,
+            to,
+            amount
+        );
 
         // Transfer the amount.
         balances[msg.sender] -= amount;
@@ -64,4 +76,3 @@ contract Token {
         return balances[account];
     }
 }
-
